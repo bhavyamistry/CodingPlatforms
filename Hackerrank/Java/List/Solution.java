f2f65b3b5dd4c6abcb736fb83ef5cae5c3c904d4
BHAVYA MISTRY <58860047+bhavyamistry@users.noreply.github.com>
2024-02-07 23:44:18 -0500

    Create Solution.java

---
+++ Hackerrank/Java/List/Solution.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            arr.add(sc.nextInt());
+        }
+        int q = sc.nextInt();
+        for(int i=0;i<q;i++){
+            String query = sc.next();
+            int pos = sc.nextInt();
+            // System.out.println(query);
+            if(query.matches("Insert")){
+                int val = sc.nextInt();
+                arr.add(pos,val);
+            }
+            else{
+                // System.out.println(pos);
+                arr.remove(pos);
+            }
+        }
+        for(int i=0;i<arr.size();i++){
+          System.out.print(arr.get(i)+" ");  
+        }
+        
+        
+    }
+}
